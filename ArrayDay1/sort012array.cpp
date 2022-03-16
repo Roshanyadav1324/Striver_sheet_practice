e6619dd40c6a1efa29b83f96608db6974dc9a1a8
Roshanyadav1324 <62843947+Roshanyadav1324@users.noreply.github.com>
2022-03-16 07:31:40 +0530

    Add files via upload

---
+++ ArrayDay1/sort012array.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int l = 0;
+    int mid = 0; //mid
+    int h = n-1;
+
+    while(mid<=h)
+    {
+        switch(arr[mid])
+        {
+            case 0:
+            {
+                swap(arr[l++], arr[mid++]);
+                break;
+            }
+
+            case 1:
+            {
+                mid++;
+                break;
+            }
+            case 2:
+            {
+                swap(arr[mid],arr[h--]);
+                break;
+            }
+        }
+    }
+
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
